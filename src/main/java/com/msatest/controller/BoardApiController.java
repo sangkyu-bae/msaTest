f481ddbd95ccc1372410a108ce8e89f1b723c3c4
배상규 <94222693+sangkyu-bae@users.noreply.github.com>
2023-01-05 20:57:16 +0900

    kafka 연동

--- src/main/java/com/msatest/controller/BoardApiController.java
+++ src/main/java/com/msatest/controller/BoardApiController.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @RestController
 public class BoardApiController {
-
     @GetMapping("/hello")
     public String hello(){
         return "hello";
